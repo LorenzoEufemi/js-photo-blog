58a8bffd71790562548da3ddda7cba009090d6ea
LorenzoEufemi <lorenzoeufemi@gmail.com>
2024-11-19 17:18:13 +0100

    feat: finalizzazione bonus.2

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
 
 const containerElem = document.querySelector(".container");
 
+const btnElem = document.querySelector(".btn");
+
+const cardImgElem = document.querySelector(".card-img");
+
+const cardTextElem = document.querySelector(".card-text");
+
 // ESECUZIONE
 
 
@@ -9,9 +15,9 @@ let result = "";
 
 const photos6 = () => { // funzione per creare 6 card con le 6 foto create dall'api
 
-    photo.forEach(curPhoto => {  //ciclo forEach per array in cui a ogni iterazione aggiung card foto e assegno tutto a result
+    photo.forEach((curPhoto, index) => {  //ciclo forEach per array in cui a ogni iterazione aggiung card foto e assegno tutto a result
         result +=
-            `<div class="card">
+            `<div class="card" data-card="${index}">
                <img class="btn-pin" src="./img/pin.svg" alt="immagine puntina ferma foto">
                <div class="card-img">
                  <img src=${curPhoto.url} alt="">
@@ -19,8 +25,9 @@ const photos6 = () => { // funzione per creare 6 card con le 6 foto create dall'
                <div class="card-text">
                  ${curPhoto.title}
                </div>
+               <button class="btn" onclick="deleteCard(${index})">cancella</button> 
              </div>`
-
+        // fatto con aiuto di Loris per quanto riguarda la parte del bonus che mi ha assegnato lui
     });
 };
 
@@ -38,3 +45,7 @@ axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6").then(resp => {
 });
 
 
+function deleteCard(index) {
+
+    document.querySelector('[data-card="' + index + '"]').remove();
+}
